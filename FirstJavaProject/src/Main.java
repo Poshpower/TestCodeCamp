a264b6893164862d4614722ea82f44016a214331
Poshpower <98832125+Poshpower@users.noreply.github.com>
2022-11-27 00:23:10 +0200

    Introduction To Java for Task 14 added

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("This is the beginning of Java Training in the Switch to software testing training");
+
+        System.out.println("Module one of the Switch to software testing give insight on whats software testing is all about, the difference between software testing and Quality assurance and many more");
+
+        System.out.println("Module Two gives a deep learning of Manual testing generally, How to write a test case Document, how to write a test case summary, Test Summary report and Bug reporting document");
+
+        System.out.println("Module Three lunched us into the Automation part of the software testing, Api test and Automation using postman and many more");
+
+        System.out.println("Thanks for coming to my Ted Talk");
+
+    }
+}
